c24fb6b43580f229e3e3a82fe1ce3950c3609f30
rdayok <r.dayok@native.semicolon.africa>
2024-01-13 12:18:22 +0100

    fix the bug for the function that searches a list if an element exist therein

--- array_snaks_java_questions/check_if_element_exist.py
+++ array_snaks_java_questions/check_if_element_exist.py
@@ -1,12 +1,9 @@
-def does_element_exist(inputted_list=[], element_checked=None):
+def does_element_exist(inputted_list, element_checked=None):
     for element in inputted_list:
         if element == element_checked:
-            exist = True
-            return exist
-        else:
-            exist = False
-            return exist
+            return True
+    return False
 
 
 lst = [25, 10, 15, 5, 30, 55, 200, 45]
-print(does_element_exist(lst, 300))
+print(does_element_exist(lst, 200))
