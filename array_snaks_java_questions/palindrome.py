682d3c0b7dc07094ffd901aa994a34ef88beefaa
rdayok <r.dayok@native.semicolon.africa>
2024-01-13 12:52:04 +0100

    refactored the application that checks if an inputted word is palindrome

--- array_snaks_java_questions/palindrome.py
+++ array_snaks_java_questions/palindrome.py
@@ -1,16 +1,14 @@
 
 
-def check_if_palindrome(input):
-    swap = userinput[:: -1]
-    lowerswap = swap.lower()
-    finalword = lowerswap.title()
+def is_palindrome(user_input):
+    swap = user_input[:: -1]
+    swap = swap.lower()
 
-    if finalword == userinput:
-        print(finalword + ' is a palindrome')
-    if finalword != userinput:
-        print(userinput + ' is not a palindrome')
+    if swap == user_input.lower():
+        print(user_input + ' is a palindrome')
+    else:
+        print(user_input + ' is not a palindrome')
 
 
 userinput = input("Enter a word to know if it is a palindrome\n")
-check_if_palindrome(userinput)
-
+is_palindrome(userinput)
