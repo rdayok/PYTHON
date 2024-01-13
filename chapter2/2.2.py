45dea3efe4b0cfecf13597095569e5bc8efa7b8d
rdayok <r.dayok@native.semicolon.africa>
2024-01-13 13:21:09 +0100

    fixed the bug of chapter2 question 2.2 task

--- chapter2/2.2.py
+++ chapter2/2.2.py
@@ -1,4 +1,5 @@
-number = input("Input a number between 1 and 10")
+number = int(input("Input a number between 1 and 10: "))
 square = number * number
+print("The square root of", number, "is ", square)
 
 # This script will throw a traceback because input() function takes in strings and not integers
