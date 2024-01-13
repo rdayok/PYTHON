9d394f13042189aa0734f93df2f08a937858164e
rdayok <r.dayok@native.semicolon.africa>
2024-01-13 13:36:49 +0100

    refactored chapter2 question 2.3 task

--- chapter2/2.3.py
+++ chapter2/2.3.py
@@ -1,3 +1,4 @@
 grade = 91
 if grade >= 90:
-    print('Congratulations! Your grade of 91 earns you an A in this course')
+    print("Congratulations! Your grade of", grade, "earns you an A in this course")
+
